6cd80323a9f9a5ac7f77a11e258ce4b8c0bb238f
marewane-fertikh <129042875+marewane-fertikh@users.noreply.github.com>
2023-03-27 10:29:19 +0200

    Update HelloWord.java
    git clone https://github.com/pierre-gerard/hello-world

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -1,8 +1,9 @@
 
-public class HelloWorld {
+public class HelloWord {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
 	}
 
 }
+git clone https://github.com/pierre-gerard/hello-world
